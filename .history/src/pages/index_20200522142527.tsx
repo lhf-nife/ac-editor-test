09083e577e3bc1b15ebec631e77d6530dc248c54
lhf-nife <lhf6157@163.com>
2020-05-22 14:31:24 +0800

    update

---
+++ .history/src/pages/index_20200522142527.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './index.less';
+
+import { AcEditorSany } from 'ac-editor-sany';
+import 'ac-editor-sany/dist/index.css';
+
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+
+      <AcEditorSany
+        // 组件id
+        editorId="acEditorSanyId"
+        // 设置ref属性
+        onRef={ref => {
+          console.log(ref, 'ref--------');
+          // child = ref;
+        }}
+        fixedDate={[]}
+      />
+    </div>
+  );
+}
